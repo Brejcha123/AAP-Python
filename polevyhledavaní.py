787761ee75e0bbd38eb5c0a052449ed5ff46bf02
Brejcha123 <jan.brejcha@infis.cz>
2025-05-13 08:37:53 +0200

    Add files via upload

---
+++ polevyhledavaní.py
@@ -0,0 +1,13 @@
+cisla = []
+
+kolikrat = int(input("Kolik čísel chcete přidat?"))
+for i in range(1, kolikrat + 1, 1):
+    cisla.append(int(input("Zadej číslo: ")))
+
+hledane = int(input("Jaké číslo chce najít?"))
+
+if hledane in cisla:
+    print("Číslo: ", hledane ," se nachází v poli")
+
+else:
+     
