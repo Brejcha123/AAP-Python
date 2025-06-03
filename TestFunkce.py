e670a131bb1f63e26cbb882db48b76500bc63efd
Brejcha123 <jan.brejcha@infis.cz>
2025-06-03 14:57:06 +0200

    Add files via upload

---
+++ TestFunkce.py
@@ -0,0 +1,19 @@
+def uvitej():
+    print("Vítej v Python světě!")
+
+def moje_mesto():
+    print("Nýřany")
+
+def tri_krat():
+    for i in range(3):
+        print("ahoj")
+
+def program_info():
+    print("Program má 3 funkce")
+    print("Umí napsat a zavolat určité funkce")
+    print("Trvala mi napsat 6 minut")
+
+uvitej()
+moje_mesto()
+tri_krat()
+program_info()
